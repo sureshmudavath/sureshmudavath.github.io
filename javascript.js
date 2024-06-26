5c886d14ae027da7b420c6eacaad9fededa23755
Suresh Mudavath <mudavathsuresh0506@gmail.com>
2024-06-26 01:02:32 -0500

    style.css
    done with portfolio

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// script.js
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        if (section.id === sectionId) {
+            section.style.display = 'block';
+        } else {
+            section.style.display = 'none';
+        }
+    });
+}
